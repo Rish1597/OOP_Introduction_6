e8d356b54fb9aed260d094a702c6153b2b1d991e
Rishabh Singh <96013245+Rish1597@users.noreply.github.com>
2024-03-05 15:50:40 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace ClassActivity_Inheritence
+{
+    public class Vehicle
+    {
+        //two properties of class vehicles
+        public string Vehicle_Registration_Number { get; set; }
+        public string Vehicle_Owner { get; set; }
+
+        // Constructor
+        public Vehicle(string Vehicle_Registration_Number, string Vehicle_Owner)
+        {
+            this.Vehicle_Registration_Number = Vehicle_Registration_Number;
+            this.Vehicle_Owner = Vehicle_Owner;
+        }
+
+        //2 behaviors of class vehicles
+
+    }
+
+    public class Car : Vehicle
+    {
+        // 2 properties of class car
+        public string Car_Manufacturer { get; set; }
+        public string Car_Color { get; set; }
+
+        //constructor
+
+        public Car(string Vehicle_Registration_Number, string Vehicle_Owner, string Car_Manufacturer, string Car_Color) : base(Vehicle_Registration_Number, Vehicle_Owner)
+        {
+            this.Car_Manufacturer = Car_Manufacturer;
+            this.Car_Color = Car_Color;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System;
+using ClassActivity_Inheritence;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        Vehicle V1 = new Vehicle("1234", "John");
+        Car C1 = new Car("12345","Ron","BMW", "Red");
+
+        }
+
+    }
